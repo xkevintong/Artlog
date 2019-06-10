080c3fca92498566428bf959fa2de530563311e4
Kevin Tong <kevin_tong100@hotmail.com>
2019-06-10 01:49:22 -0700

    WIP writing URLs to json

--- parser.py
+++ parser.py
@@ -1,36 +1,82 @@
+import json
+
 from bs4 import BeautifulSoup
 import requests
+from tldextract import extract
 
-links = open('links.txt', 'w', encoding='utf8')
-fb_page = open('fb_page.txt', 'w', encoding='utf8')
-scratch = open('scratch.txt', 'w', encoding='utf8')
 
-soup = BeautifulSoup(open('tini.html', encoding='utf8'), 'html.parser')
+def request_url_from_div(div):
+    # Check for class="touchable _4qxt" 's href, which should be most links?
+    if div.find(class_='touchable _4qxt') is not None:
+        url = div.find(class_='touchable _4qxt')['href']
+        response = requests.get(url)
+        response_soup = BeautifulSoup(response.content, 'html.parser')
 
-message_group = soup.find('div', {'id' : 'messageGroup'})
+        # Facebook's redirection page for websites outside their domain
+        return response_soup.find('span', class_='_5slv').get_text()
 
-msgs = message_group.find_all('div', class_='msg')
 
-num_divs = 0
+def get_url_from_msg(msg):
+    # Check for class="touchable _4qxt" 's href, which should be most links?
+    if msg.div.span.a is not None:
+        return "".join(msg.div.span.a.find_all(text=True))
 
-for i, msg in enumerate(msgs):
-	for div in msg.find_all('div', recursive=False):
-		num_divs += 1
-		scratch.write(div.prettify())
+    # Facebook page
+    elif msg.find(class_="story_body_container") is not None:
+        return msg.find(class_="_39pi")["href"]
 
-		# Check for class="touchable _4qxt" 's href, which should be most links?
-		if div.find(class_='touchable _4qxt') is not None:
-			url = div.find(class_='touchable _4qxt')['href']
-			response = requests.get(url)
-			response_soup = BeautifulSoup(response.content, 'html.parser')
-			links.write(response_soup.find('span', class_='_5slv').get_text() + '\n')
+    return None
 
-		# Check for FB page links
 
-		# Check for images
+def parse():
+    links_file = open('links.json', 'w', encoding='utf8')
+    scratch = open('scratch.txt', 'w', encoding='utf8')
 
-		# Everything else should go in some Misc. section
+    soup = BeautifulSoup(open('tini.html', encoding='utf8'), 'html.parser')
+    message_group = soup.find('div', {'id': 'messageGroup'})
+    msgs = message_group.find_all('div', class_='msg')
 
-print(i+1)
-print(num_divs)
+    num_divs = 0
+    valid_domains = ["pixiv", "twitter", "reddit", "artstation", "deviantart"]
+    links = {}
+    for domain in valid_domains:
+        links[domain] = []
 
+    # "misc" is not a valid domain, but will collect
+    links["misc"] = []
+
+    for i, msg in enumerate(msgs):
+        num_divs += 1
+        scratch.write(msg.prettify())
+
+        # Write to json, then write once to file
+        url = get_url_from_msg(msg)
+        if url:
+            domain = extract(url).domain
+            if domain in valid_domains:
+                links[domain].append(url)
+            else:
+                links["misc"].append(url)
+        else:
+            print("Nothing extracted from div")
+            print(msg.prettify())
+
+    json.dump(links, links_file, indent=4)
+
+    # Check for links to facebook pages
+    # Look for story_body_container
+
+    # Check for raw images
+
+    # Text messages have content within span
+
+    # Everything else should go in some Misc. section
+    # is there really a misc section if i have to know how to pull the url
+    # really just a have to sort manually section
+
+    if i+1 != num_divs:
+        print(f'{num_divs-i-1} messages were not correctly processed')
+
+
+if __name__ == "__main__":
+    parse()
