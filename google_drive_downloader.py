9bc0becd4f934a5015e5436d0111fd0b46c28e55
Kevin Tong <kevin_tong100@hotmail.com>
2019-10-13 00:14:55 -0700

    blacked files

--- google_drive_downloader.py
+++ google_drive_downloader.py
@@ -16,31 +16,36 @@ import pendulum
 
 
 def get_drive_service():
-    scopes = 'https://www.googleapis.com/auth/drive'
-    store = file.Storage('storage.json')
+    scopes = "https://www.googleapis.com/auth/drive"
+    store = file.Storage("storage.json")
     creds = store.get()
     if not creds or creds.invalid:
-        flow = client.flow_from_clientsecrets('client_id.json', scopes)
+        flow = client.flow_from_clientsecrets("client_id.json", scopes)
         creds = tools.run_flow(flow, store)
-    return discovery.build('drive', 'v3', http=creds.authorize(Http()))
+    return discovery.build("drive", "v3", http=creds.authorize(Http()))
 
 
 def extract_url_from_file(bytes):
-    file_string = bytes.decode('utf-8')
-    if 'https://www.pixiv.net' in file_string:
-        return 'pixiv', file_string.strip()
-    elif 'https://twitter.com' in file_string:
-        return 'twitter', file_string[file_string.rfind('https://twitter.com'):]
+    file_string = bytes.decode("utf-8")
+    if "https://www.pixiv.net" in file_string:
+        return "pixiv", file_string.strip()
+    elif "https://twitter.com" in file_string:
+        return "twitter", file_string[file_string.rfind("https://twitter.com") :]
     else:
-        return 'misc', file_string
+        return "misc", file_string
 
 
 def download_files_from_drive(service):
-    folder_response = service.files().list(
-        q="mimeType = 'application/vnd.google-apps.folder' and name = 'Artlog'",
-        spaces='drive',
-        fields='files(id)').execute()
-    folder_id = folder_response.get('files', []).pop()['id']
+    folder_response = (
+        service.files()
+        .list(
+            q="mimeType = 'application/vnd.google-apps.folder' and name = 'Artlog'",
+            spaces="drive",
+            fields="files(id)",
+        )
+        .execute()
+    )
+    folder_id = folder_response.get("files", []).pop()["id"]
 
     pixiv_list = []
     twitter_list = []
@@ -48,12 +53,18 @@ def download_files_from_drive(service):
 
     page_token = None
     while True:
-        file_response = service.files().list(q=f"parents in '{folder_id}'",
-                                             spaces='drive',
-                                             fields='nextPageToken, files(id, createdTime)',
-                                             pageToken=page_token).execute()
-        for drive_file in file_response.get('files', []):
-            download_request = service.files().get_media(fileId=drive_file.get('id'))
+        file_response = (
+            service.files()
+            .list(
+                q=f"parents in '{folder_id}'",
+                spaces="drive",
+                fields="nextPageToken, files(id, createdTime)",
+                pageToken=page_token,
+            )
+            .execute()
+        )
+        for drive_file in file_response.get("files", []):
+            download_request = service.files().get_media(fileId=drive_file.get("id"))
             fh = io.BytesIO()
             downloader = MediaIoBaseDownload(fh, download_request)
             done = False
@@ -61,22 +72,29 @@ def download_files_from_drive(service):
                 status, done = downloader.next_chunk()
 
             website, url = extract_url_from_file(fh.getvalue())
-            formatted_time = pendulum.parse(
-                drive_file.get('createdTime')).in_tz('America/Los_Angeles').format('YYYY-MM-DD HH:mm')
-            if website == 'pixiv':
-                pixiv_list.append({'url': url, 'time': formatted_time})
-            elif website == 'twitter':
-                twitter_list.append({'url': url, 'time': formatted_time})
-            elif website == 'misc':
-                misc_list.append({'url': url, 'time': formatted_time})
+            formatted_time = (
+                pendulum.parse(drive_file.get("createdTime"))
+                .in_tz("America/Los_Angeles")
+                .format("YYYY-MM-DD HH:mm")
+            )
+            if website == "pixiv":
+                pixiv_list.append({"url": url, "time": formatted_time})
+            elif website == "twitter":
+                twitter_list.append({"url": url, "time": formatted_time})
+            elif website == "misc":
+                misc_list.append({"url": url, "time": formatted_time})
             print(url, formatted_time)
-        page_token = file_response.get('nextPageToken', None)
+        page_token = file_response.get("nextPageToken", None)
         if page_token is None:
             break
 
-    print({'pixiv': pixiv_list, 'twitter': twitter_list})
-    with open('drive_links.json', 'w') as links:
-        json.dump({'pixiv': pixiv_list, 'twitter': twitter_list, 'misc': misc_list}, links, indent=4)
+    print({"pixiv": pixiv_list, "twitter": twitter_list})
+    with open("drive_links.json", "w") as links:
+        json.dump(
+            {"pixiv": pixiv_list, "twitter": twitter_list, "misc": misc_list},
+            links,
+            indent=4,
+        )
 
 
 def get_urls_from_drive():
--- parser.py
+++ parser.py
@@ -11,13 +11,13 @@ from credentials import *
 
 def request_url_from_div(div):
     # Check for class="touchable _4qxt" 's href, which should be most links?
-    if div.find(class_='touchable _4qxt'):
-        url = div.find(class_='touchable _4qxt')['href']
+    if div.find(class_="touchable _4qxt"):
+        url = div.find(class_="touchable _4qxt")["href"]
         response = requests.get(url)
-        response_soup = BeautifulSoup(response.content, 'html.parser')
+        response_soup = BeautifulSoup(response.content, "html.parser")
 
         # Facebook's redirection page for websites outside their domain
-        redirected_domain = response_soup.find('span', class_='_5slv')
+        redirected_domain = response_soup.find("span", class_="_5slv")
         if redirected_domain:
             return redirected_domain.get_text()
         else:
@@ -47,22 +47,29 @@ def get_url_from_msg(msg, domains):
 
 
 def get_time_from_msg(div):
-    if div.find('abbr'):
-        unix_time = json.loads(div.find('abbr')['data-store'])['time']
+    if div.find("abbr"):
+        unix_time = json.loads(div.find("abbr")["data-store"])["time"]
         time = arrow.Arrow.fromtimestamp(unix_time)
-        return time.to('US/Pacific').format('YYYY-MM-DD HH:mm')
+        return time.to("US/Pacific").format("YYYY-MM-DD HH:mm")
 
 
 def parse_html():
-    links_file = open('links.json', 'w', encoding='utf8')
-    scratch = open('scratch.txt', 'w', encoding='utf8')
+    links_file = open("links.json", "w", encoding="utf8")
+    scratch = open("scratch.txt", "w", encoding="utf8")
 
-    soup = BeautifulSoup(open('messenger/smol.html', encoding='utf8'), 'html.parser')
-    message_group = soup.find('div', {'id': 'messageGroup'})
-    msgs = message_group.find_all('div', class_='c')
+    soup = BeautifulSoup(open("messenger/smol.html", encoding="utf8"), "html.parser")
+    message_group = soup.find("div", {"id": "messageGroup"})
+    msgs = message_group.find_all("div", class_="c")
 
     num_divs = 0
-    valid_domains = ["pixiv", "twitter", "reddit", "artstation", "deviantart", "facebook"]
+    valid_domains = [
+        "pixiv",
+        "twitter",
+        "reddit",
+        "artstation",
+        "deviantart",
+        "facebook",
+    ]
     links = {}
     for domain in valid_domains:
         links[domain] = []
@@ -76,12 +83,12 @@ def parse_html():
         num_divs += 1
         scratch.write(msg.prettify())
         info = {
-            'url': get_url_from_msg(msg.div, valid_domains),
-            'time': get_time_from_msg(msg.div.next_sibling)
+            "url": get_url_from_msg(msg.div, valid_domains),
+            "time": get_time_from_msg(msg.div.next_sibling),
         }
 
-        if info['url']:
-            domain = extract(info['url']).domain
+        if info["url"]:
+            domain = extract(info["url"]).domain
             if domain in valid_domains:
                 links[domain].append(info)
             else:
@@ -99,8 +106,8 @@ def parse_html():
     # is there really a misc section if i have to know how to pull the url
     # really just a have to sort manually section
 
-    if i+1 != num_divs:
-        print(f'{num_divs-i-1} messages were not correctly processed')
+    if i + 1 != num_divs:
+        print(f"{num_divs-i-1} messages were not correctly processed")
     else:
         print("All messages processed correctly!")
 
@@ -123,20 +130,22 @@ def twitter_test():
 
 def _requests_twitter_download(tweet_url):
     response = requests.get(tweet_url)
-    response_soup = BeautifulSoup(response.content, 'html.parser')
-    image_source_div = response_soup.find('div', class_='AdaptiveMedia-photoContainer js-adaptive-photo')
+    response_soup = BeautifulSoup(response.content, "html.parser")
+    image_source_div = response_soup.find(
+        "div", class_="AdaptiveMedia-photoContainer js-adaptive-photo"
+    )
     if image_source_div:
-        image_source = image_source_div['data-image-url']
-        image_name = image_source.rsplit('/', 1)[-1]
+        image_source = image_source_div["data-image-url"]
+        image_name = image_source.rsplit("/", 1)[-1]
 
         # The large image's path can be reached by appending ':large'
-        image_response = requests.get(image_source + ':large')
-        with open(f'images\{image_name}', 'wb') as file:
+        image_response = requests.get(image_source + ":large")
+        with open(f"images\{image_name}", "wb") as file:
             file.write(image_response.content)
-        return 'Success', image_source
+        return "Success", image_source
     else:
         print("Image failed to download: " + tweet_url)
-        return 'Failed', None
+        return "Failed", None
 
 
 def like_tweet(tweepy_api, tweet_id):
@@ -145,56 +154,58 @@ def like_tweet(tweepy_api, tweet_id):
 
 def download_twitter_images():
     api = twitter_auth()
-    with open('links.json') as links:
+    with open("links.json") as links:
         image_status = []
-        for msg in json.load(links)['twitter']:
-            tweet_id = msg['url'].partition('status/')[2].partition('?')[0]
+        for msg in json.load(links)["twitter"]:
+            tweet_id = msg["url"].partition("status/")[2].partition("?")[0]
             num_media = 0
             if tweet_id:
                 tweet = api.get_status(tweet_id)._json
-                if not tweet['favorited']:
+                if not tweet["favorited"]:
                     try:
                         # todo: helper func these
-                        for media in tweet['extended_entities']['media']:
+                        for media in tweet["extended_entities"]["media"]:
                             num_media += 1
-                            msg['source'] = media_source = media['media_url']
-                            media_name = media['media_url'].rsplit('/', 1)[-1]
-                            media_response = requests.get(media_source + ':large')
-                            with open(f'images\{media_name}', 'wb') as file:
+                            msg["source"] = media_source = media["media_url"]
+                            media_name = media["media_url"].rsplit("/", 1)[-1]
+                            media_response = requests.get(media_source + ":large")
+                            with open(f"images\{media_name}", "wb") as file:
                                 file.write(media_response.content)
-                            print(media['media_url'])
+                            print(media["media_url"])
                     except KeyError:
                         pass
                     # Also check to see if a tweet was quoted
                     try:
-                        for media in tweet['quoted_status']['extended_entities']['media']:
+                        for media in tweet["quoted_status"]["extended_entities"][
+                            "media"
+                        ]:
                             num_media += 1
-                            msg['source'] = media_source = media['media_url']
-                            media_name = media['media_url'].rsplit('/', 1)[-1]
-                            media_response = requests.get(media_source + ':large')
-                            with open(f'images\{media_name}', 'wb') as file:
+                            msg["source"] = media_source = media["media_url"]
+                            media_name = media["media_url"].rsplit("/", 1)[-1]
+                            media_response = requests.get(media_source + ":large")
+                            with open(f"images\{media_name}", "wb") as file:
                                 file.write(media_response.content)
-                            print(media['media_url'])
+                            print(media["media_url"])
                     except KeyError:
                         pass
 
                     # Like the tweet after downloading
-                    msg['already_liked'] = False
+                    msg["already_liked"] = False
                     if num_media > 0:
                         like_tweet(api, tweet_id)
 
                 else:
                     num_media = -1
-                    msg['already_liked'] = True
+                    msg["already_liked"] = True
             else:
-                msg['error'] = "Not a tweet"
-            msg['num_media'] = num_media
+                msg["error"] = "Not a tweet"
+            msg["num_media"] = num_media
             # msg['status'], msg['source'] = _requests_twitter_download(msg['url'])
-            msg['id'] = tweet_id
-            msg['time_downloaded'] = arrow.now('US/Pacific').format('YYYY-MM-DD HH:mm')
+            msg["id"] = tweet_id
+            msg["time_downloaded"] = arrow.now("US/Pacific").format("YYYY-MM-DD HH:mm")
             image_status.append(msg)
 
-    with open('twitter.json', 'w') as twitter:
+    with open("twitter.json", "w") as twitter:
         json.dump(image_status, twitter, indent=4)
 
 
